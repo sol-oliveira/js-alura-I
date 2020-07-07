9f19154ec58d06964b8a42cd94c250dfae78f9c7
Solemar Silva <solemar.silva@tmax.com.br>
2020-07-07 17:53:09 -0300

    Arrow function e seu escopo léxico

--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,18 +1,15 @@
 class ListaNegociacoes {
     
-    constructor(contexto, armadilha) {
-        
+    constructor(armadilha) {
+
         this._negociacoes = [];
-        this._armadilha = armadilha;
-        this._contexto = contexto;
+        this._armadilha = armadilha;       
     }
     
     adiciona(negociacao) {
         
         this._negociacoes.push(negociacao);
-      //  this._armadilha(this);
-       	Reflect.apply(this._armadilha, this._contexto, [this]);
-
+        this._armadilha(this);    
     }
     
     get negociacoes() {
@@ -23,8 +20,6 @@ class ListaNegociacoes {
     esvazia() {
 
    	 	this._negociacoes = [];
-   	 //	this._armadilha(this);
-   	 	 Reflect.apply(this._armadilha, this._contexto, [this]);
-
-	}
+   		this._armadilha(this);
+   	}
 }
