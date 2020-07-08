d8ae587a45162d1e8614746e8e6454de624bd5fa
Solemar Silva <solemar.silva@tmax.com.br>
2020-07-08 17:55:01 -0300

    realizando o parse da resposta

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -31,22 +31,21 @@ class NegociacaoController {
 
         /* configurações */
         xhr.open('GET', 'negociacoes/semana');
-
-        /*  0: requisição ainda não iniciada
-
-            1: conexão com o servidor estabelecida
-
-            2: requisição recebida
-
-            3: processando requisição
-
-            4: requisição está concluída e a resposta está pronta*/
+       
         xhr.onreadystatechange = () => {
         if(xhr.readyState == 4) {
+            
             if(xhr.status == 200) {
-                 console.log('Obtendo as negociações do servidor.');        
+         
+                JSON.parse(xhr.responseText)
+                .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                this._mensagem.texto = 'Negociações importadas com sucesso.';
+     
             }else{
-                console.log('Não foi possível obter as negociações do servidor.');        
+                this._mensagem.texto = 'Não foi possível obter as negociações.'; 
+                console.log(xhr.responseText);
+        
             }
         }
     };
