a8cf69345c7fa46e98b3f222185863342103107e
Solemar Silva <solemar.silva@tmax.com.br>
2020-07-08 15:10:53 -0300

    padrão de Projeto Factory

---
+++ aluraframe/client/js/app/services/ProxyFactory.js
@@ -0,0 +1,16 @@
+class ProxyFactory {
+  static create(objeto, props, acao) {
+  	return new Proxy(objeto, {
+            get: function(target, prop, receiver) {              
+                if(props.includes(prop) && typeof(target[prop]) == typeof(Function)) {
+                    return function() {
+                        console.log(`a propriedade "${prop}" foi interceptada`);
+                        Reflect.apply(target[prop], target, arguments); 
+                        acao(target);
+                    }
+                }
+                return Reflect.get(target, prop, receiver);
+            }        
+        });
+  	}
+}
