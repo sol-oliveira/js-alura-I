f66838d393ed2f8dc3c838b7764f018a04938cc6
Solemar Silva <solemar.silva@tmax.com.br>
2020-07-08 15:20:37 -0300

    refatorando o Proxy

--- aluraframe/client/js/app/services/ProxyFactory.js
+++ aluraframe/client/js/app/services/ProxyFactory.js
@@ -2,7 +2,7 @@ class ProxyFactory {
   static create(objeto, props, acao) {
   	return new Proxy(objeto, {
             get: function(target, prop, receiver) {              
-                if(props.includes(prop) && typeof(target[prop]) == typeof(Function)) {
+                if(props.includes(prop) && ProxyFactory._ehFuncao(target[prop])) {
                     return function() {
                         console.log(`a propriedade "${prop}" foi interceptada`);
                         Reflect.apply(target[prop], target, arguments); 
@@ -10,7 +10,19 @@ class ProxyFactory {
                     }
                 }
                 return Reflect.get(target, prop, receiver);
-            }        
+            } ,
+            set(target, prop, value, receiver) {
+                if(props.includes(prop)) {
+                    target[prop] = value;
+                    acao(target);
+                }
+  				return Reflect.set(target, prop, value, receiver);   				 
+			}       
         });
   	}
+
+  	static _ehFuncao(func) {
+    	return typeof(func) == typeof(Function);
+	}
+
 }
