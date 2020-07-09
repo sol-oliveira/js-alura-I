fc43b1d240abc01dd1b455cde5e2c1c9a7148c10
Solemar Silva <solemar.silva@tmax.com.br>
2020-07-09 17:13:48 -0300

    criar a classe  ConnectionFactory

---
+++ aluraframe/client/js/app/services/ConnectionFactory.js
@@ -0,0 +1,29 @@
+var stores = ['negociacoes'];
+var version = 4;
+var dbName = 'aluraframe';
+
+class ConnectionFactory {
+
+    constructor() {
+
+        throw new Error('Não é possível criar instâncias de ConnectionFactory');
+    }
+
+    static getConnection() {
+
+        return new Promise((resolve, reject) => {
+
+        	let openRequest = window.indexedDB.open('aluraframe',4);  
+
+        	openRequest.onupgradeneeded = e => {
+	        };
+
+	        openRequest.onsuccess = e => {    
+	        };
+
+	        openRequest.onerror = e => {    
+	        };  
+
+        });
+    }
+}
