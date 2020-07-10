ee65f8dc9921d4510c47b0605345f9c4fa299507
Solemar Silva <solemar.silva@tmax.com.br>
2020-07-10 11:56:18 -0300

    importar negociações automaticamente

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -18,18 +18,26 @@ class NegociacaoController {
             new MensagemView($('#mensagemView')),
             'texto');  
 
-       ConnectionFactory
-        .getConnection()
-        .then(connection => new NegociacaoDao(connection))
-        .then(dao => dao.listaTodos())        
-        .then(negociacoes =>
-            negociacoes.forEach(negociacao =>
-                this._listaNegociacoes.adiciona(negociacao)))
-        .catch(erro => {
-        this._mensagem.texto = erro;
-      });
+        this._init();
+    }
 
+    _init() {
 
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.listaTodos())
+            .then(negociacoes =>
+                negociacoes.forEach(negociacao =>
+                  this._listaNegociacoes.adiciona(negociacao)))
+            .catch(erro => {
+                console.log(erro);
+                this._mensagem.texto = erro;
+            });
+
+            setInterval(() => {
+                this.importaNegociacoes();
+            }, 3000);
     }
     
     adiciona(event) {
