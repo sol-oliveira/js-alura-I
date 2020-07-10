c693d6ce72b312a1a161899914b3ed686c0313b0
Solemar Silva <solemar.silva@tmax.com.br>
2020-07-10 09:12:10 -0300

    monkey Patch

--- aluraframe/client/js/app/services/ConnectionFactory.js
+++ aluraframe/client/js/app/services/ConnectionFactory.js
@@ -1,10 +1,11 @@
-// Module Pattern. U
 var ConnectionFactory = (function(){
 
-var stores = ['negociacoes'];
-var version = 4;
-var dbName = 'aluraframe';
+const stores = ['negociacoes'];
+const version = 4;
+const dbName = 'aluraframe';
 var connection = null;
+var close = null;
+
 
 	return class ConnectionFactory {
 
@@ -25,7 +26,14 @@ var connection = null;
 	            };
 
 	            openRequest.onsuccess = e => { 
-	                if(!connection) connection = e.target.result;
+	                if(!connection){
+	                	connection = e.target.result	                	
+	                	close = connection.close.bind(connection);
+	                	//Monkey Patch,       
+	                	 connection.close = function() {
+							throw new Error('Você não pode fechar diretamente a conexão');
+        				};
+	                } 
 	                resolve(connection);
 	            };
 
@@ -45,6 +53,15 @@ var connection = null;
 	            connection.createObjectStore(store, { autoIncrement: true });
 	        });
 	    }
+
+	    static closeConnection(){
+
+		    if(connection){		    	 
+		        close();
+		        connection = null;
+
+		    }
+		}
 	}
 
 }) ();
